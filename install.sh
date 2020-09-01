36f225b7899559dc0e350ee9f26cf9c57985a708
wonder2k <46643182+wonder2k@users.noreply.github.com>
2020-09-01 14:45:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ EOF
 
 clone_repo(){
     echo "进行初始化。。。"
-    git clone https://github.com/CCChieh/IBMYes
+    git clone https://github.com/wonder2k/IBMYes
     cd IBMYes
     git submodule update --init --recursive
     cd v2ray-cloudfoundry/v2ray
